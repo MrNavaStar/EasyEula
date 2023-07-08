0fa46ccd730a75db5e552fa8c78371840d12cfc9
MrNavaStar <Mr.NavaStar@gmail.com>
2023-07-07 19:51:55 -0600

    updates

--- src/main/java/me/mrnavastar/easyeula/EasyEula.java
+++ src/main/java/me/mrnavastar/easyeula/EasyEula.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
 public class EasyEula {
-    public static final String MOD_ID = "EasyEula";
+    public static final String MOD_ID = "Easy Eula";
 
     public static void log(Level level, String message) {
         LogManager.getLogger().log(level, "[" + MOD_ID + "] " + message);
