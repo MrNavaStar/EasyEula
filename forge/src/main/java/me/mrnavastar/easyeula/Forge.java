6dacec76bd1c74d28875b94d4b9674641f900b0f
MrNavaStar <Mr.NavaStar@gmail.com>
2024-02-28 02:56:20 -0700

    pog forge works

---
+++ forge/src/main/java/me/mrnavastar/easyeula/Forge.java
@@ -0,0 +1,7 @@
+package me.mrnavastar.easyeula;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("easyeula")
+public class Forge {
+}
--- src/main/java/me/mrnavastar/easyeula/Forge.java
+++
@@ -1,8 +0,0 @@
-package me.mrnavastar.easyeula;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(EasyEula.MOD_ID)
-public class Forge {
-
-}
